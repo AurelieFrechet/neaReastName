04fe209c4ad4cb7a363614eeb77c7b0be89fc84c
Aurelie Frechet <aurelie.frechet@gmail.com>
2021-01-22 21:42:28 +0100

    ajoute de la documentation

--- R/pertinence.R
+++ R/pertinence.R
@@ -1,11 +1,10 @@
-#' Title
-#'
+#' pertinence
+#' @description This is the section where I'm supposed
+#' to describe this f** functions I've spend my evening on
 #' @param vector result of common_names
 #'
-#' @return
+#' @return pertinence score between 0 and 1
 #' @export
-#'
-#' @examples
 pertinence <- function(names_found, center_names_w = 0.1){
   n = length(names_found)
   names_weight    = rep(center_names_w, n)
