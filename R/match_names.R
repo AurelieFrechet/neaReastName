b97d000fd9c1590c4a98925460ee90b30bd0cc3b
Aurelie Frechet <aurelie.frechet@gmail.com>
2021-01-30 20:46:07 +0100

    change the score

--- R/match_names.R
+++ R/match_names.R
@@ -1,6 +1,21 @@
+#' @include common_names.R
+#' @include is_similar.R
+#' @title match_names
+#' @description match names
+#'
+#' @param name name to match
+#' @param list_of_names list of names
+#' @param threshold threshold of distance
+#' @param r_letters letters to change
+#' @param last_name TRUE if last name is in last position,
+#' else, last name is in first position
+#'
+#' @return list of matches
+#' @export
 match_names <- function(name,
                         list_of_names,
                         threshold = 1,
+                        last_name = TRUE,
                         r_letters = c(
                           "K" = "C",
                           "W" = "V",
@@ -8,54 +23,63 @@ match_names <- function(name,
                           "Z" = "S"
                         ),
                         center_names_w = 0.1) {
-
   score <- sapply(
     X = list_of_names,
     FUN = function(n) {
-        # Full name, ordered
-        0.4 * pertinence(common_names(
+      # If all names concat are exactly the same
+      if (is_similar(
+        name1     = paste(name, collapse = ""),
+        name2     = paste(n, collapse = ""),
+        threshold = 0,
+        r_letters = r_letters
+      )) {
+        return(1)
+
+      } else {
+        # Exact Names non-matched
+        exact_names_ordered <- common_names(
           name1 = name,
           name2 = n,
-          initials_only = FALSE,
+          order = TRUE,
+          threshold = 0,
+          r_letters = r_letters
+        )
+        exact_names_NA <- sum(is.na(exact_names_ordered))
+
+        # Exact Names non-matched
+        similar_names_ordered <- common_names(
+          name1 = name,
+          name2 = n,
           order = TRUE,
           threshold = threshold,
           r_letters = r_letters
-        )) +
-        # Full name, non-ordered
-        0.4 * pertinence(common_names(
-          name1 = name,
-          name2 = n,
-          initials_only = FALSE,
-          order = TRUE,
-          threshold = threshold,
-          r_letters = r_letters
-        )) +
+        )
+        similar_names_NA <- sum(is.na(similar_names_ordered))
 
-        # Initials only, ordered
-        0.1 * pertinence(common_names(
-          name1 = name,
-          name2 = n,
-          initials_only = TRUE,
-          order = TRUE,
-          threshold = threshold,
-          r_letters = r_letters
-        )) +
+        # Number of names
+        n_names <- length(exact_names_ordered)
+        # Position of last name
+        if (last_name) {
+          last_name_pos <- n_names
+        } else {
+          last_name_pos <- 1
+        }
 
-        # Initials only, non-ordered
-        0.1 * pertinence(common_names(
-          name1 = name,
-          name2 = n,
-          initials_only = TRUE,
-          order = TRUE,
-          threshold = threshold,
-          r_letters = r_letters
-        )) +
+        # If lastname exactly the same (threshold = 0)
+        if (!is.na(exact_names_ordered[last_name_pos])) {
+          return(1 - (exact_names_NA + similar_names_NA) / (n_names * 2))
+        } else {
+          # If last name is similar
+          if (!is.na(similar_names_ordered[last_name_pos])) {
+            return(1 - ((exact_names_NA - 1) + similar_names_NA) / ((n_names - 1) * 2))
+          }
+          return(0)
+        }
+      }
 
-        (concat_namedist(name1 = name,
-                               name2 = n,
-                               r_letters = r_letters) <= threshold)
-         })
 
-return(list_of_names[score > 0.5])
+    }
+  )
+  return(list_of_names[score >= 0.5])
 
 }
--- tests/testthat/test-match_names.R
+++ tests/testthat/test-match_names.R
@@ -13,14 +13,23 @@ list_of_names = list(
   c("ALICIA", "MARIE", "MARLENE"),
   c("BART", "MANN"),
   c("MARIE", "LILY", "MARLEEN"),
-  c("LILI", "MARLENE")
+  c("LILI", "MARLENE"),
+  c("ABOT", "EUGENE"),
+  c("ABOT", "EUGENE", "MICHEL", "JOSEPH"),
+  c("ABOUT", "SUZANNE"),
+  c("ABEL", "PUJOL", "FILS", "ALEXANDRE"),
+  c("ABEL", "PUJOL", "ALEXANDRE", "DENIS"),
+  c("ABEL", "PUJOL", "ADRIENNE", "MARIE", "LOUISE", "GRANDPIERRE", "DEVERZY"),
+  c("ABEL", "DUFRESNE"),
+  c("L", "MARLENE")
 )
 
 
 test_that("LILI MARLENE", {
   expect_equal(match_names(
     name = c("LILI", "MARLENE"),
-    list_of_names = list_of_names
+    list_of_names = list_of_names,
+    last_name = TRUE
   ),
   list(c("MARIE", "LILY", "MARLEEN"),
        c("LILI", "MARLENE")))
@@ -33,5 +42,17 @@ test_that("BATMAN", {
                            list_of_names = list_of_names),
                list(c("BATMAN"),
                     c("BAT", "MAN")))
+})
+
+test_that("ABOT ABOUT", {
+  expect_equal(match_names(name = c("ABOT", "EUGENE", "MICHEL", "JOSEPH"),
+                           list_of_names = list_of_names,
+                           last_name = FALSE),
+               list(c("ABOT", "EUGENE"),
+                    c("ABOT", "EUGENE", "MICHEL", "JOSEPH")))
+  expect_equal(match_names(name = c("ABOUT", "SUZANNE"),
+                           list_of_names = list_of_names,
+                           last_name = FALSE),
+               list(c("ABOUT", "SUZANNE")))
 })
 
